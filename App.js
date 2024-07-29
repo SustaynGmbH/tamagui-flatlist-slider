16412811231630bf6bdfcb1c37de06ab207320e0
greenGizmo <jan@sustayn.de>
2024-07-29 14:59:33 +0200

    mre

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
+import { config } from '@tamagui/config/v3';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
+import { createTamagui, Slider, TamaguiProvider } from 'tamagui';
+
+const tamaguiConfig = createTamagui(config);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <TamaguiProvider config={tamaguiConfig}>
+            <View style={styles.container}>
+                <FlatList
+                    data={Array(20)}
+                    renderItem={(item) => (
+                        <View style={{ width: 350, height: 50, margin: 15 }}>
+                            <Slider size={30} width={300} defaultValue={[40]} max={100} step={1}>
+                                <Slider.Track height={5}>
+                                    <Slider.TrackActive backgroundColor="yellowgreen" />
+                                </Slider.Track>
+                                <Slider.Thumb circular index={0} />
+                            </Slider>
+                        </View>
+                    )}
+                />
+            </View>
+            <StatusBar style="auto" />
+        </TamaguiProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        marginTop: 100,
+        backgroundColor: '#fff',
+        width: '100%',
+        height: '100%',
+    },
 });
